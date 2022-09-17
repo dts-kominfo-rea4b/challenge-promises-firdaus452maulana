4f17d5b5c26021748c8fd41769f1d6a84fd66147
Firdaus <firdausmaulana@akupintar.id>
2022-09-17 15:19:42 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = (emosi) => {
+  return new Promise((resolve) => {
+    let count = 0;
+    promiseTheaterIXX().then((value) => {
+      value.forEach(element => {
+        if (element.hasil === emosi) count+=1
+      });
+      // console.log(value)
+      return promiseTheaterVGC()
+    }).then((value) => {
+      value.forEach(element => {
+        if (element.hasil === emosi) count+=1
+      });
+      // console.log(value2)
+      resolve(count)
+    })
+  })
+}
 
 module.exports = {
   promiseOutput,
